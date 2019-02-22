fe2986421c0627f33763f1147a9d00671f39fde0
Rahulmallampati825 <rahulmallampati825@gmail.com>
2019-02-22 16:24:40 -0600

    erty

--- function.js
+++ function.js
@@ -2,5 +2,13 @@ function math() {
 	var radius = document.getElementById("radius").value;
 	var height = document.getElementById("height").value;
 	var out = document.getElementById("out");
-	out.value = Math.PI*radius*radius*height;
+	out.value = Math.PI * radius * radius * height;
+	const v = Math.PI * radius * radius * height;
+	localStorage.setItem("store", v)
 }
+
+console.log('Creating the local storage')
+const add = document.getElementById("store");
+add.addEventListener('click', function () {
+	document.getElementById("save").innerHTML = localStorage.getItem("store")
+})
