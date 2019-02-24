000b581e4868d004ee91a3c4883ac75b26919269
Rahulmallampati825 <rahulmallampati825@gmail.com>
2019-02-23 20:22:10 -0600

    gf

--- function.js
+++ function.js
@@ -1,17 +1,20 @@
-function math() {
-	var radius = document.getElementById("radius").value;
-	var height = document.getElementById("height").value;
-	var out = document.getElementById("out");
-	out.value = Math.PI * radius * radius * height;
-	const v = Math.PI * radius * radius * height;
-	localStorage.setItem("store", v)
-}
+// function math() {
+// 	var radius = document.getElementById("radius").value;
+// 	var height = document.getElementById("height").value;
+// 	// var out = document.getElementById("out");
+// 	// out.value = Math.PI * radius * radius * height;
+// 	const v = Math.PI * radius * radius * height;
+// 	localStorage.setItem("store", v)
+// }
 
-console.log('Creating the local storage')
-const add = document.getElementById("store");
-add.addEventListener('click', function () {
-	document.getElementById("save").innerHTML = localStorage.getItem("store")
-})
+// console.log('Creating the local storage')
+// const add = document.getElementById("store");
+// add.addEventListener('click', function () {
+// 	document.getElementById("save").innerHTML = localStorage.getItem("store")
+// })
+// function store(){
+// 	document.getElementById('storage').innerHTML = "volume of cylinder" + localStorage.getItem("store")
+// }
 
 var R = new XMLHttpRequest();
 R.open('GET', 'https://codepen.io/mallampati-rahul/pen/ErqdGr.html');
